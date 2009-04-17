2ebd97d3cd5f78ad980e8748133b31d43ec15ba9
Nicolas Doualot <public@slubman.info>
2009-04-18 01:29:39 +0800

    More "generic" bang.sh for mac OS X : just change the LOCALDIR
    Signed-off-by: Mike Desjardins <desjardinsmike@gmail.com>

--- bang.sh
+++ bang.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+LOCALDIR=~/Projects
 sudo rm -fr /Applications/Buzzbird.app 
-sudo /Library/Frameworks/XUL.framework/xulrunner-bin --install-app "/Users/mdesjardins/_play/buzzbird" 
-sudo cp /Users/mdesjardins/_play/buzzbird/Info.plist /Applications/Buzzbird.app/Contents
+sudo /Library/Frameworks/XUL.framework/xulrunner-bin --install-app "${LOCALDIR}/buzzbird" 
+sudo cp ${LOCALDIR}/buzzbird/Info.plist /Applications/Buzzbird.app/Contents
 sudo mv /Applications/Buzzbird.app/Contents/Resources/chrome/content/global/macOverlay.xul /Applications/Buzzbird.app/Contents/Resources/chrome/content/global/platformOverlay.xul 
 /Applications/Buzzbird.app/Contents/MacOS/xulrunner -jsconsole -console
