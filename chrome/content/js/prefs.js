e00ca7090e411af0b1ef802f91b9effbf90080f8
Mike Desjardins <desjardinsmike@gmail.com>
2009-06-03 21:14:30 -0400

    More small changes to support multiple accounts (a long time coming)

--- chrome/content/js/prefs.js
+++ chrome/content/js/prefs.js
@@ -7,6 +7,37 @@ var httprealm = null;
 var user = '';
 var password = '';
 
+function loginListClicked() {
+	var deleteButton = document.getElementById('deleteAccount');
+	var listbox = document.getElementById('richlistbox_accounts');
+	//alert('x:' + listbox.selectIndex);
+	if (listbox.selectedIndex >= 0) {
+		deleteButton.disabled = false;
+	} else {
+		deleteButton.disabled = true;
+	}
+}
+
+function addAccount() {
+	var params = {};
+	window.openDialog("chrome://buzzbird/content/add-account.xul", "",
+	    "chrome, dialog, modal, resizable=no",params).focus();
+	if (params.out) {
+		var login = params.out.login;
+		var password = params.out.password;
+		alert(login + ", " + password);
+	}
+}
+
+function onAddAccountOk() {
+	window.arguments[0].out = {login:document.getElementById("login").value, password:document.getElementById("password").value};
+	return true;
+}
+
+function onAddAccountCancel() {
+	return true;
+}
+
 function initLogins() {
 	try {
 		// Get Login Manager 
@@ -26,7 +57,8 @@ function initLogins() {
 			var len = logins.length;
 		   	if (logins != null && len > 0) {
 				for (i=0; i<len; i++) {
-			 		listbox.appendItem(logins[i].username);
+					var value = logins[i].username + "||" + logins[i].password + "||" + logins[i].hostname + "||" + logins[i].formSubmitURL;
+			 		listbox.appendItem(logins[i].username,value);
 				}
 			} else {
 		     jsdump('No saved logins found.');	
