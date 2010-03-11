5a3a8587df522d9211eaf55a3f6f28138287703b
Rick van der Zwet <info@rickvanderzwet.n>
2010-03-11 13:34:00 +0100

    Allow calling the application from various location and different architectures

--- bang.bat
+++ bang.bat
@@ -2,4 +2,4 @@
 xcopy chrome\skin\classic-win\* chrome\skin\classic /Y /S /Q
 copy chrome\content\global\windowsOverlay.xul chrome\content\global\platformOverlay.xul
 copy chrome\content\js\notify\notify-win.js chrome\content\js\notify\notify.js
-start "buzzbird" "C:\Program Files (x86)\xulrunner\xulrunner.exe" .\application.ini -jsconsole
+start "buzzbird" "%PROGRAMFILES%\xulrunner\xulrunner.exe" %~dp0\application.ini -jsconsole
