f30f1908d7c45a224ee9a13c5d56846f4187f818
Rick van der Zwet <info@rickvanderzwet.nl>
2010-03-11 13:52:50 +0100

    Damm the copy was also using relative paths

--- bang.bat
+++ bang.bat
@@ -1,5 +1,6 @@
 @echo off
+cd %~dp0
 xcopy chrome\skin\classic-win\* chrome\skin\classic /Y /S /Q
 copy chrome\content\global\windowsOverlay.xul chrome\content\global\platformOverlay.xul
 copy chrome\content\js\notify\notify-win.js chrome\content\js\notify\notify.js
-start "buzzbird" "%PROGRAMFILES%\xulrunner\xulrunner.exe" %~dp0\application.ini -jsconsole
+start "buzzbird" "%PROGRAMFILES%\xulrunner\xulrunner.exe" application.ini -jsconsole
