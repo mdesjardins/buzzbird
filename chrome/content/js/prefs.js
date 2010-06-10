003e653394558b9bc6576c530e46e799d5e019d4
Mike Desjardins <desjardinsmike@gmail.com>
2010-06-10 10:12:09 -0400

    Added support for proxies!

--- chrome/content/js/prefs.js
+++ chrome/content/js/prefs.js
@@ -156,7 +156,7 @@ var Prefs = {
 			xulAccount.setAttribute('hidden','false');
 			xulAccount.getElementsByAttribute('field','image')[0].setAttribute('src','http://a1.twimg.com/profile_images/687800820/Buzzbird.png');
 			xulAccount.getElementsByAttribute('field','image')[0].setAttribute('img_screen_name',account.username);
-			xulAccount.getElementsByAttribute('field','screen_name')[0].setAttribute('value',account.username);
+			xulAccount.getElementsByAttribute('field','screen_name')[0].setAttribute('value',account.username + '@' + account.service);
 			xulAccount.getElementsByAttribute('field','real_name')[0].setAttribute('screen_name',account.username);
 			xulAccount.setAttribute('value',value);
 			Social.service("twitter").fetchUserProfile({
@@ -215,6 +215,129 @@ var Prefs = {
 		Prefs.setSticky('directVisualAlert',false);		
 	}
 }
+
+//
+// Below was ripped off from Firefox and tweaked, so all the stuff below 
+// this line is licensed under the MPL.
+//
+var Proxy = {
+  beforeAccept: function ()
+  {
+    var proxyTypePref = document.getElementById("network.proxy.type");
+    if (proxyTypePref.value == 2) {
+      this.doAutoconfigURLFixup();
+      return true;
+    }
+
+    if (proxyTypePref.value != 1)
+      return true;
+
+    var httpProxyURLPref = document.getElementById("network.proxy.http");
+    var httpProxyPortPref = document.getElementById("network.proxy.http_port");
+    return true;
+  },
+
+  checkForSystemProxy: function ()
+  {
+    if ("@mozilla.org/system-proxy-settings;1" in Components.classes)
+      document.getElementById("systemPref").removeAttribute("hidden");
+  },
+  
+  proxyTypeChanged: function ()
+  {
+    var proxyTypePref = document.getElementById("network.proxy.type");
+    
+    // Update http
+    var httpProxyURLPref = document.getElementById("network.proxy.http");
+    httpProxyURLPref.disabled = proxyTypePref.value != 1;
+    var httpProxyPortPref = document.getElementById("network.proxy.http_port");
+    httpProxyPortPref.disabled = proxyTypePref.value != 1;
+
+    // Now update the other protocols
+    this.updateProtocolPrefs();
+
+    var autoconfigURLPref = document.getElementById("network.proxy.autoconfig_url");
+    autoconfigURLPref.disabled = proxyTypePref.value != 2;
+
+    this.updateReloadButton();
+  },
+
+  updateReloadButton: function ()
+  {
+    // Disable the "Reload PAC" button if the selected proxy type is not PAC or
+    // if the current value of the PAC textbox does not match the value stored
+    // in prefs.  Likewise, disable the reload button if PAC is not configured
+    // in prefs.
+
+    var typedURL = document.getElementById("networkProxyAutoconfigURL").value;
+    var proxyTypeCur = document.getElementById("network.proxy.type").value;
+
+    var prefs =
+        Components.classes["@mozilla.org/preferences-service;1"].
+        getService(Components.interfaces.nsIPrefBranch);
+    var pacURL = prefs.getCharPref("network.proxy.autoconfig_url");
+    var proxyType = prefs.getIntPref("network.proxy.type");
+
+    var disableReloadPref =
+        document.getElementById("pref.advanced.proxies.disable_button.reload");
+    disableReloadPref.disabled =
+        (proxyTypeCur != 2 || proxyType != 2 || typedURL != pacURL);
+  },
+  
+  readProxyType: function ()
+  {
+    this.proxyTypeChanged();
+    return undefined;
+  },
+  
+  updateProtocolPrefs: function ()
+  {
+    var proxyTypePref = document.getElementById("network.proxy.type");
+    var proxyPrefs = ["ssl"];
+    for (var i = 0; i < proxyPrefs.length; ++i) {
+      var proxyServerURLPref = document.getElementById("network.proxy." + proxyPrefs[i]);
+      var proxyPortPref = document.getElementById("network.proxy." + proxyPrefs[i] + "_port");
+      proxyServerURLPref.updateElements();
+      proxyPortPref.updateElements();
+      proxyServerURLPref.disabled = proxyTypePref.value != 1;
+      proxyPortPref.disabled = proxyServerURLPref.disabled;
+    }
+    var socksVersionPref = document.getElementById("network.proxy.socks_version");
+    socksVersionPref.disabled = proxyTypePref.value != 1;
+    
+    return undefined;
+  },
+  
+  reloadPAC: function ()
+  {
+    Components.classes["@mozilla.org/network/protocol-proxy-service;1"].
+        getService().reloadPAC();
+  },
+  
+  doAutoconfigURLFixup: function ()
+  {
+    var autoURL = document.getElementById("networkProxyAutoconfigURL");
+    var autoURLPref = document.getElementById("network.proxy.autoconfig_url");
+    var URIFixup = Components.classes["@mozilla.org/docshell/urifixup;1"]
+                             .getService(Components.interfaces.nsIURIFixup);
+    try {
+      autoURLPref.value = autoURL.value = URIFixup.createFixupURI(autoURL.value, 0).spec;
+    } catch(ex) {}
+  },
+  
+  readHTTPProxyServer: function ()
+  {
+    this.updateProtocolPrefs();
+    return undefined;
+  },
+  
+  readHTTPProxyPort: function ()
+  {
+    this.updateProtocolPrefs();
+    return undefined;
+  }
+};
+
 
 window.addEventListener("load", Prefs.initLogins, false);
 window.addEventListener("load", Prefs.initSticky, false);
